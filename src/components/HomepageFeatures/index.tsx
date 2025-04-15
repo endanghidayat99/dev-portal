8150494082ead3548f80e7868611d68c810014c4
endanghidayat0912 <endang.hidayat0912@gmail.com>
2025-04-15 14:49:23 +0700

    init

---
+++ src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,84 @@
+import type {ReactNode} from 'react';
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+type FeatureItem = {
+  title: string;
+  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  link: string;
+  description: ReactNode;
+};
+
+const FeatureList: FeatureItem[] = [
+  {
+    title: 'Gitlab',
+    Svg: require('@site/static/img/gitlab.svg').default,
+      link: 'https://cbsc-git-repo.collega.co.id/',
+    description: (
+      <>
+        Docusaurus was designed from the ground up to be easily installed and
+        used to get your website up and running quickly.
+      </>
+    ),
+  },
+  {
+    title: 'Wiki',
+    Svg: require('@site/static/img/wiki.svg').default,
+      link:'',
+    description: (
+      <>
+        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
+        ahead and move your docs into the <code>docs</code> directory.
+      </>
+    ),
+  },
+  {
+    title: 'Nexus Repo',
+    Svg: require('@site/static/img/nexus.svg').default, link:'http://192.168.212.42:8081',
+    description: (
+      <>
+        Extend or customize your website layout by reusing React. Docusaurus can
+        be extended while reusing the same header and footer.
+      </>
+    ),
+  },
+    {
+        title: 'Komodo',
+        Svg: require('@site/static/img/komodo.svg').default, link:'http://192.168.212.43:9120/',
+        description: (
+            <>
+                Extend or customize your website layout by reusing React. Docusaurus can
+                be extended while reusing the same header and footer.
+            </>
+        ),
+    },
+];
+
+function Feature({title, Svg, description, link}: FeatureItem) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+
+        <a href={link} target='_blank' ><Heading as="h3">{title}</Heading></a>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures(): ReactNode {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
