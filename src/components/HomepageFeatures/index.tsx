e5a3551d7bce4d8f2c11af9aa7eaf4c9cbcf1c1c
endang hidayat <endang.hidayat@collega.co.id>
2025-07-15 11:19:20 +0700

    add rsa

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -113,6 +113,16 @@ const FeatureList: FeatureItem[] = [
             </>
         ),
     },
+    {
+        title: 'RSA',
+        Svg: require('@site/static/img/rsa.svg').default, link:'http://192.168.212.124:3411/RsaUtilWebUI',
+        description: (
+            <>
+                Extend or customize your website layout by reusing React. Docusaurus can
+                be extended while reusing the same header and footer.
+            </>
+        ),
+    },
 ];
 
 function Feature({title, Svg, description, link}: FeatureItem) {
